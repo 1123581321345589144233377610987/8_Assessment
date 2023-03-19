aac08f099732a7a4d89fcf960c9fd652f67da057
1123581321345589144233377610987 <123220105+1123581321345589144233377610987@users.noreply.github.com>
2023-03-19 00:42:14 -0500

    Update 8.1_Assessment.py

--- 8.1_Assessment.py
+++ 8.1_Assessment.py
@@ -1,19 +1,22 @@
-errorfile = "accounts.txt"
+afile = "accounts.txt"
+odfile = "over90.txt"
 try:
-    Accounts = open('accounts.txt','r')
-    errorfile = "over90.txt"
-    Due = open('over90.txt','r')
-    accounts = Accounts.readlines()
-    due = Due.readlines()
-    Accounts.close()
-    Due.close()
-    count = 0
-    for i in due:
-        due[count] = due[count].strip('\n')
-        count += 1
-    for i in accounts:
-        I = i.split(',')
-        if I[0] in due:
-            print(i.strip("\n)
+    Accounts = open(afile,'r')
+    try:
+        Due = open(odfile,'r')
+        accounts = Accounts.readlines()
+        due = Due.readlines()
+        Accounts.close()
+        Due.close()
+        count = 0
+        for i in due:
+            due[count] = due[count].strip('\n')
+            count += 1
+        for i in accounts:
+            I = i.split(',')
+            if I[0] in due:
+                print(i.strip("\n"))
+    except FileNotFoundError:
+        print(f"{odfile} does not exist.")
 except FileNotFoundError:
-    print(f"{errorfile} does not exist.")
+    print(f"{afile} does not exist.")
