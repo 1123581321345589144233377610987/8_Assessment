4b54c973bcce3d72867cd5e35b7e514fc07475ba
1123581321345589144233377610987 <123220105+1123581321345589144233377610987@users.noreply.github.com>
2023-03-18 23:14:09 -0500

    Add files via upload

---
+++ 8.1_Assessment.py
@@ -0,0 +1,19 @@
+errorfile = "accounts.txt"
+try:
+    Accounts = open('accounts.txt','r')
+    errorfile = "over90.txt"
+    Due = open('over90.txt','r')
+    accounts = Accounts.readlines()
+    due = Due.readlines()
+    Accounts.close()
+    Due.close()
+    count = 0
+    for i in due:
+        due[count] = due[count].strip('\n')
+        count += 1
+    for i in accounts:
+        I = i.split(',')
+        if I[0] in due:
+            print(i)
+except FileNotFoundError:
+    print(f"{errorfile} does not exist.")
